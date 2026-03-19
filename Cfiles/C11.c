80d768e14300adefe33b22cac80ad3ffa4215362
cs136l <chenyinwilliam@gmail.com>
2026-03-19 18:27:17 -0400

    m

--- Cfiles/C11.c
+++ Cfiles/C11.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 
-int binsearch(int A[], int key, int n) {
+int lowsearch(int A[], int key, int n) {
+    //assume: A[0] <= key <= A[n-1]
     int low = 0;
-    int high = n - 1;
-    while (low <= high) {
+    int high = n;
+    // low = 0, high = n
+    // let i be the minimum index such that key <= A[i]
+
+    while (low < high) {
+        // loop invariant: low(before) <= i <= high(before) -> low(after) <= i <= high(after)
         int mid = (low + high) / 2;
+        // mid = (low+high)/2, low <= mid < high
         if (key < A[mid]) {
+            // key < A[mid] -> 
+            // i < mid -> 
+            // i <= mid-1
             high = mid - 1;
+            // low <= i <= high (mid-1)
         } else if (key > A[mid]) {
+            // key > A[mid] -> 
+            // i > mid -> 
+            // i >= mid+1
             low = mid + 1;
+            // low (mid+1) <= i <= high
         } else {
-            return mid;
+            // key = A[mid] -> 
+            // i <= mid
+            high = mid;
+            // low <= i <= high (mid)
         }
     }
-    return n;
+    // low <= i <= high AND low = high
+    // therefore low = i
+
+    return low;
 }
 
 void main() {
-    int arr[] = {1, 3, 3, 5, 5, 7, 7, 7, 9, 11, 13};
-    printf("%d", binsearch(arr, 4, 11));
+    int arr[] = {};
+    printf("\n%d\n", lowsearch(arr, 0, 0));
 }
